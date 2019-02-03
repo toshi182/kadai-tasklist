39e3a1414f7eb539500acf4f5454b52b1cb33535
EC2 Default User <ec2-user@ip-172-31-64-68.ec2.internal>
2019-02-03 10:29:44 +0000

    bootstrap design

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,60 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @tasks = Task.find(params[:id])
+  end
+  
+  def new
+    @tasks = Task.new
+  end
+  
+  def create
+    @tasks = Task.new(task_params)
+
+  if @tasks.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @tasks
+  else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+  end
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.find(params[:id])
+
+    if @tasks.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @tasks
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :new
+    end
+
+  end
+
+  def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+
+  private
+  def set_task
+    @tasks = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:content,:status)
+  end
+end
