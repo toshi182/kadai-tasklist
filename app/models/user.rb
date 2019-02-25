b6b920e3083a5eb66cb7e44d95a71f3c0c6a1b75
toshihisa <toshi19920613@yahoo.co.jp>
2019-02-25 13:56:35 +0000

    user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
+                    
+  
+  has_many:tasks
+  
 end
