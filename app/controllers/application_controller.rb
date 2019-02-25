271b7972a6f2a8f22762e63885222ffc92475261
toshihisa <toshi19920613@yahoo.co.jp>
2019-02-25 10:15:14 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   
   def require_user_logged_in
     unless logged_in?
-      require_to login_url
+      redirect_to login_url
     end
   end
 end
