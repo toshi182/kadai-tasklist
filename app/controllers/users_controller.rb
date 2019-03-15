60761b9710519467b40c98df4cf687a4cea76c12
toshihisa <toshi19920613@yahoo.co.jp>
2019-03-15 12:31:13 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to current_user
+    end
     @users = @user.tasks.order('created_at DESC').page(params[:page])
     counts(@user)
   end
